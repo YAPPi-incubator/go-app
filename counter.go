a5235b2b4ff3852179616a7b00f18dcce4479875
AJ <aj@48k.io>
2017-10-28 09:37:26 +0100

    removing semicolons, renaming incr -> inc

--- counter.go
+++ counter.go
@@ -5,26 +5,26 @@ import (
 	"net/http"
 )
 
-var number int = 0
+var number = 0
 
 func get(response http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(response, "%d\n", number)
 }
 
-func incr(response http.ResponseWriter, request *http.Request) {
-	number = number + 1;
+func inc(response http.ResponseWriter, request *http.Request) {
+	number = number + 1
 	fmt.Fprintf(response, "%d\n", number)
 }
 
 func reset(response http.ResponseWriter, request *http.Request) {
-	number = 0;
+	number = 0
 	fmt.Fprintf(response, "%d\n", number)
 }
 
 func main() {
 	http.HandleFunc("/",      get)
 	http.HandleFunc("/get",   get)
-	http.HandleFunc("/incr",  incr)
+	http.HandleFunc("/inc", inc)
 	http.HandleFunc("/reset", reset)
 	http.ListenAndServe(":8080", nil)
 }
